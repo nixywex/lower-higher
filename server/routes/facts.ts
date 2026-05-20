151348052cfb17ceb0b9ffe88594f04f933e4d0f
nixywex <nixywex@gmail.com>
2026-05-20 15:20:47 +0200

    feat: return facts without answers

--- server/routes/facts.ts
+++ server/routes/facts.ts
@@ -11,27 +11,46 @@ interface Fact {
   answer: number;
 }
 
+interface FactForClient {
+    id: number;
+    question: string;
+}
+
 const factsFilePath: string = "./facts.json";
 
 const router = Router();
 
 router.get("/round", async (_, res) => {
   try {
-    const facts = await readFacts(factsFilePath);
-
+    const facts = await getClientFacts();
     res.status(200).send(facts);
   } catch (error) {
     console.error("Error reading facts file: ", error);
-
     res.status(500).send("Unexpected exception");
   }
 });
 
-async function readFacts(filePath: string): Promise<File> {
+async function getClientFacts(): Promise<FactForClient[]> {
+    const facts = await readFacts(factsFilePath);
+
+    //TODO: get 7 random of them
+
+    const factsForClient = facts.map(fact => {
+        var factForClient: FactForClient = {
+            id: fact.id,
+            question: fact.question
+        }
+        return factForClient;
+    })
+
+    return factsForClient;
+}
+
+async function readFacts(filePath: string): Promise<[Fact]> {
   const jsonString = await fs.promises.readFile(filePath, "utf8");
   const data: File = JSON.parse(jsonString);
 
-  return data;
+  return data.facts;
 }
 
 export default router;
