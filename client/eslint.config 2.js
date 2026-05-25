9fb11f04de98c32aaab86b353d1ab3bee80835cc
nixywex <nixywex@gmail.com>
2026-05-25 16:15:46 +0200

    chore: setup es lint and prettier

--- client/eslint.config 2.js
+++ client/eslint.config 2.js
@@ -4,7 +4,6 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,7 +14,6 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      prettierConfig,
     ],
     languageOptions: {
       globals: globals.browser,
