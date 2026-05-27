e2ab1a7fad8c6f6c1941cc6535aa6e2922fca57d
Natalija Urošević <natalijaurosevic@MacBook-Air-von-Natalija.local>
2026-05-27 15:23:47 +0200

    Start-Page

---
+++ client/src/pages/StartPage.tsx
@@ -0,0 +1,13 @@
+import './StartPage.css';
+
+function StartPage() {
+  return (
+    <div className="container">
+      <h1>Higher-Lower</h1>
+      <p>Welcome to the Higher-Lower game!</p>
+      <button className="start-button">Singleplayer</button>
+    </div>
+  );
+}
+
+export default StartPage;
