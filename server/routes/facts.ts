70e66bb21a66142e47287d1ddb34bc486eb60cfa
nixywex <nixywex@gmail.com>
2026-05-30 17:44:30 +0200

    fix: fix scoring method

--- server/routes/facts.ts
+++ server/routes/facts.ts
@@ -1,31 +1,27 @@
-import { Router } from "express";
-import { calculateScore } from "../utils/scoring";
-import { getRightAnswers, getClientFacts } from "../utils/facts_handling";
+import { Router } from 'express';
+import { calculateScore } from '../utils/scoring';
+import { getRightAnswers, getClientFacts } from '../utils/facts_handling';
 
 const router = Router();
 const numberOfFacts = 7;
 
-router.get("/round", async (_, res) => {
+router.get('/round', async (_, res) => {
   try {
     const facts = await getClientFacts(numberOfFacts);
 
     res.status(200).send(facts);
   } catch (error) {
-    console.error("Error reading facts file: ", error);
-    res.status(500).send("Unexpected exception");
+    console.error('Error reading facts file: ', error);
+    res.status(500).send('Unexpected exception');
   }
 });
 
-router.post("/submit", async (req, res) => {
+router.post('/submit', async (req, res) => {
   try {
     const { ids }: { ids: number[] } = req.body;
     const rightAnswers = await getRightAnswers(ids);
 
-    const score = calculateScore(
-      rightAnswers.map((fact) => fact.answer),
-      ids,
-      10000,
-    );
+    const score = calculateScore(rightAnswers, ids, 10000);
 
     //TODO: object for response?
     res.status(200).send({
@@ -33,8 +29,8 @@ router.post("/submit", async (req, res) => {
       score: score,
     });
   } catch (error) {
-    console.error("Error submitting the results: ", error);
-    res.status(500).send("Unexpected exception");
+    console.error('Error submitting the results: ', error);
+    res.status(500).send('Unexpected exception');
   }
 });
 
--- server/utils/storing.ts
+++ server/utils/storing.ts
@@ -1,13 +1,18 @@
-import type { Fact, File } from "../types/index";
-import fs from "fs";
+import type { Fact, File } from '../types/index';
+import fs from 'fs';
 
-const factsFilePath: string = "../facts.json";
+const factsFilePath: string = '../facts.json';
 
 export async function getAllFacts(): Promise<Fact[]> {
-  const jsonString = await fs.promises.readFile(factsFilePath, "utf8");
+  const jsonString = await fs.promises.readFile(factsFilePath, 'utf8');
   const data: File = JSON.parse(jsonString);
 
   return data.facts;
 }
 
-module.exports = { getAllFacts };
+export async function getFactsByIds(ids: number[]): Promise<Fact[]> {
+  const allFacts = await getAllFacts();
+  return allFacts.filter((fact) => ids.includes(fact.id));
+}
+
+module.exports = { getAllFacts, getFactsByIds };
